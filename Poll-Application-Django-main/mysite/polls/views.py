a95d063a0e73f247adaa68d881fa8a2913c5fa33
samuelkalubi <samuel.kalubi@4-sure.net>
2023-02-02 11:54:32 +0200

    my django polls app

--- Poll-Application-Django-main/mysite/polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-
-
-def index(request):
-    return HttpResponse("Hello, world. This is the main page")
-
-def polls(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
